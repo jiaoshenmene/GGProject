5a321e651059c9533d57cf9520c5ce6d7ac01999
sogou <815319775@qq.com>
2016-05-29 19:23:51 +0800

    pod 集成

--- GGProject/ViewController.m
+++ GGProject/ViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "HomeVC.h"
 
 @interface ViewController ()
+@property (nonatomic , strong) HomeVC *homeVC;
 
 @end
 
@@ -17,6 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _homeVC = [[HomeVC alloc] init];
+    _homeVC.view.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    
+    [self.view addSubview:_homeVC.view];
+    
 }
 
 - (void)didReceiveMemoryWarning {
---
+++ Podfile
@@ -0,0 +1,5 @@
+source 'https://github.com/CocoaPods/Specs.git'
+source 'https://github.com/jiaoshenmene/Specs12.git'
+platform :ios, '7.0'
+
+pod ‘DJHomeFramework’
